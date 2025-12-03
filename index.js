851f19e8102c12c4d441858884fe4fd43fa4c7eb
Thane <thabay0606@students.maquoketaschools.org>
2025-12-03 14:21:31 -0600

    Stripped away function portion of loop

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const person = new Person(John, Stevens);
 
 const guests = [];
 
-function guestCounter(guests) {
+
   for (let i = 0; i < 3; i++) {
     guests.push(person.firstName, person.lastName);
   }
- return guests;
-}
+
